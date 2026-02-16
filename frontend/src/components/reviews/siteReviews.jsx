8ec2f2a6468bc28ba799760065400b6512f9bbba
Akash <akashkrishnan806@gmail.com>
2026-02-17 00:51:36 +0530

    Frontned Error Clear Commit2

--- frontend/src/components/reviews/siteReviews.jsx
+++ frontend/src/components/reviews/siteReviews.jsx
@@ -4,7 +4,7 @@ import { Link } from "react-router-dom";
 
 const API_URL =  import.meta.env.VITE_API_URL
 
-import "./SiteReviews.css";
+import "./siteReviews.css";
 
 import avatar from "../images/Avatar.png";
 
